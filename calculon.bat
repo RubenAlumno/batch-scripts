26b8f2065de8989d794833d151c6375156f084d0
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2015-12-15 12:14:02 +0100

    Create calculon.bat

---
+++ calculon.bat
@@ -0,0 +1,13 @@
+:: calculon.bat
+@echo off
+
+::operaciones: + - * /
+SET  /A valor=600 + 66
+
+SET  /A otro=%valor% * 2
+SET /A  numero=333*2
+echo %valor%
+echo %numero%
+echo %otro%
+
+::esto sirve para sumar y multiplicar estos números
