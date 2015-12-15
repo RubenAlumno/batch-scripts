2419646532f790be976326751826c2f9b97bd748
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2015-12-15 09:34:04 +0100

    Create argumentos.bat

---
+++ argumentos.bat
@@ -0,0 +1,12 @@
+::argumentos.bat
+@echo off
+
+echo Me has pasado
+echo %1 y apellido %2 y tienes %3
+set nombre=%1
+set apellido=%2
+set edad=%3
+
+echo %nombre% %apellido% tiene %edad% tacos
+
+::%1 es lo que pongas despues de argumentos.bat , ejem argumentos.bat Rubén
