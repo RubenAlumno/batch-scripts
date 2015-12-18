a266819584a09ae0258c50a4ae14bf8a6ee52c5c
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2015-12-18 12:27:40 +0100

    Create borrarcarpetasdel1al10.bat

---
+++ borrarcarpetasdel1al10.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+ECHO ¿HOLA COMO ESTAS?
+MD 1 2 3 4 5 6 7 8 9 10
+ECHO "VAS A BORRAR FICHEROS"
+PAUSE
+rmdir 1
+rmdir 2
+rmdir 3
+rmdir 4
+rmdir 5
+rmdir 6
+rmdir 7
+rmdir 8
+rmdir 9
+rmdir 10
