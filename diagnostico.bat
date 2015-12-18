857e15b860f81c8c6b43d370dec38a1cbb778e9a
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2015-12-18 12:30:57 +0100

    Create diagnostico.bat

---
+++ diagnostico.bat
@@ -0,0 +1,17 @@
+@echo off
+:: La directiva @echo off quita el eco
+
+rem Esto es un comentario
+:: Esto es otro comentario, más pro
+echo Hola guapo.
+echo Vamos a hacer un diagnóstico
+pause
+
+ipconfig
+pause
+
+route print
+pause
+
+echo Ping a la puerta de enlace
+ping 172.30.0.1
