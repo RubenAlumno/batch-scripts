80ddbca3abdeed09780620f79c4531fd5b5fde1d
Rubén Castañeda Matute <ruben.berriozar@gmail.com>
2015-12-18 12:29:26 +0100

    Create condicion2.bat

---
+++ condicion2.bat
@@ -0,0 +1,19 @@
+::condicion2.bat
+@echo off
+set /p nombre=Como te llamas?
+
+
+
+if %nombre% EQU Julia (
+	ECHO Hola guapa
+) else (
+     echo fuera bicho
+ )
+ 
+ 
+ ::EQU igual y
+ ::NEQ no igual
+ ::LSS menos que
+ ::LEQ menor que o igual
+ ::GTR mayor que
+ ::GEQ mayor que o igual
